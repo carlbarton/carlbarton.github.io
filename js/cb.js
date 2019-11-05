6c7505a3ec82579a2c4bdf69c955e294872595a4
Carl Barton <56538694+carlbarton@users.noreply.github.com>
2019-11-05 22:31:28 +0000

    Add CSS and JS for flip cards (currently unused), add remaining team items, some grammar/spelling corrections

--- js/cb.js
+++ js/cb.js
@@ -6,3 +6,18 @@ $('.popover-dismiss').popover({
     trigger: 'focus'
 })
   
+//Enable flip cards on click 
+    //todo
+//Set flip cards to same height on front and back
+// var frontHeight = $('.flip-card-front').outerHeight();
+// var backHeight = $('.flip-card-back').outerHeight();
+
+// if (frontHeight > backHeight) {
+//     $('.flip-card, .flip-card-back').height(frontHeight);
+// }
+// else if (frontHeight < backHeight) {
+//     $('.flip-card, .flip-card-front').height(backHeight);
+// }
+// else {
+//     $('.flip-card').height(backHeight);
+// }
